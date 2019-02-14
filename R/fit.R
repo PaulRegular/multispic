a6dd57097c1c207222c461b4e807a7563fef79f8
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2019-02-13 23:37:55 -0330

    Make a fit function to make it easier to fit different versions of the model

---
+++ R/fit.R
@@ -0,0 +1,97 @@
+
+
+
+#' Fit a multispecies surplus production model
+#'
+#' @param inputs   List that includes landings and index data
+#' @param q_groups Name of column in the index data to group the q parameter estimates by
+#' @param cor_str  Correlation structure across species. "none" will not estimate
+#'                 correlations across species, "one" will estimate one shared correlation
+#'                 parameter across species, and "all" will estimate correlation parameters
+#'                 across all combinations of species.
+#'
+#' @return
+#' @export
+#'
+
+fit_model <- function(inputs, q_groups = "gear_season", cor_str = "one") {
+
+    landings <- inputs$landings
+    index <- inputs$index
+
+    ## Scale index and landings to aid convergence
+    scaler <- mean(index$index)
+    index$index <- index$index / scaler
+    landings$landings <- landings$landings / scaler
+
+    n_cor <- sum(lower.tri(matrix(NA, nrow = nlevels(landings$species),
+                                  ncol = nlevels(landings$species))))
+    dat <- list(L = as.numeric(landings$landings),
+                L_species = as.numeric(landings$species) - 1,
+                L_year = as.numeric(landings$y) - 1,
+                I = as.numeric(index$index),
+                I_species = as.numeric(index$species) - 1,
+                I_survey = as.numeric(index[, q_groups]) - 1,
+                I_sy = as.numeric(index$sy) - 1,
+                min_B = 0.001,
+                nY = max(as.numeric(landings$y)),
+                nS = max(as.numeric(landings$species)))
+    par <- list(log_B = matrix(0, nrow = dat$nY, ncol = dat$nS),
+                log_sd_B = rep(-1, nlevels(landings$species)),
+                logit_cor = rep(0, n_cor),
+                log_K = 2,
+                log_r = rep(-1, nlevels(landings$species)),
+                log_m = rep(log(2), nlevels(landings$species)),
+                log_q = rep(-1, nlevels(index[, q_groups])),
+                log_sd_I = rep(-1, nlevels(index[, q_groups])))
+    map <- list(log_m = factor(rep(NA, nlevels(landings$species))))
+    if (cor_str == "one") {
+        map$logit_cor <- factor(rep(1, length(par$logit_cor)))
+    }
+    if (cor_str == "none") {
+        map$logit_cor <- factor(rep(NA, length(par$logit_cor)))
+        par$logit_cor <- rep(-11, n_cor)
+    }
+
+    ## Fit model
+    obj <- MakeADFun(dat, par, map = map, random = "log_B", DLL = "multispic")
+    opt <- nlminb(obj$par, obj$fn, obj$gr,
+                  control = list(eval.max = 1000, iter.max = 1000))
+    sd_rep <- sdreport(obj)
+    rep <- obj$report()
+
+    ## Reset scale
+    landings$landings <- landings$landings * scaler
+    index$index <- index$index * scaler
+
+    ## Extract and append fits
+    est <- split(unname(sd_rep$value), names(sd_rep$value))
+    sd <- split(sd_rep$sd, names(sd_rep$value))
+    lwr <- split(unname(sd_rep$value) - 1.96 * sd_rep$sd, names(sd_rep$val))
+    upr <- split(unname(sd_rep$value) + 1.96 * sd_rep$sd, names(sd_rep$val))
+    index$pred <- exp(est$log_pred_I) * scaler
+    index$pred_lwr <- exp(lwr$log_pred_I) * scaler
+    index$pred_upr <- exp(upr$log_pred_I) * scaler
+    index$std_res <- rep$log_I_std_res
+
+    ## Extract process error
+    pe <- data.frame(year = landings$year,
+                     species = landings$species,
+                     stock = landings$stock,
+                     pe = rep$log_B_std_res)
+
+    ## Extract biomass
+    biomass <- data.frame(year = landings$year,
+                          species = landings$species,
+                          stock = landings$stock,
+                          B = exp(est$log_B_vec) * scaler,
+                          B_lwr = exp(lwr$log_B_vec) * scaler,
+                          B_upr = exp(upr$log_B_vec) * scaler)
+
+    list(scaler = scaler, obj = obj, opt = opt, sd_rep = sd_rep, rep = rep,
+         index = index, landings = landings, pe = pe, biomass = biomass)
+
+}
+
+
+
--- analysis/3LNO_case_study.R
+++ analysis/3LNO_case_study.R
@@ -1,4 +1,6 @@
 
+## WARNING: Origin changed for some reason to multispic to MSP??
+
 ## TODO:
 ## - Start comparing parameter estimates to those in the assessments
 ## - Get latest catch numbers for 3O redfish and 3LNO hake (2016 and 2017 are guesses)
@@ -15,11 +17,6 @@ unique(multispic::landings$species)
 
 index <- multispic::index
 landings <- multispic::landings
-
-## Scale index and landings to aid convergence
-scaler <- 100
-index$index <- index$index / scaler
-landings$landings <- landings$landings / scaler
 
 ## Subset the data
 sub_sp <- unique(multispic::landings$species)
@@ -51,7 +48,7 @@ p <- index %>%
 p
 p %>% layout(yaxis = list(type = "log"))
 
-
+## Exploratory plots
 index %>%
     group_by(survey) %>%
     mutate(scaled_index = scale(index)) %>%
@@ -74,122 +71,73 @@ landings %>%
     add_lines(x = ~year, y = ~total_landings)
 
 
-n_cor <- sum(lower.tri(matrix(NA, nrow = nlevels(landings$species),
-                              ncol = nlevels(landings$species))))
-dat <- list(L = as.numeric(landings$landings),
-            L_species = as.numeric(landings$species) - 1,
-            L_year = as.numeric(landings$y) - 1,
-            I = as.numeric(index$index),
-            I_species = as.numeric(index$species) - 1,
-            I_survey = as.numeric(index$gear_season) - 1,
-            I_sy = as.numeric(index$sy) - 1,
-            min_B = 0.001,
-            nY = max(as.numeric(landings$y)),
-            nS = max(as.numeric(landings$species)))
-par <- list(log_B = matrix(0, nrow = dat$nY, ncol = dat$nS),
-            log_sd_B = rep(-1, nlevels(landings$species)),
-            logit_cor = rep(0, n_cor),
-            log_K = 2,
-            log_r = rep(-1, nlevels(landings$species)),
-            log_m = rep(log(2), nlevels(landings$species)),
-            log_q = rep(-1, nlevels(index$gear_season)),
-            log_sd_I = rep(-1, nlevels(index$gear_season)))
-map <- list(log_m = factor(rep(NA, nlevels(landings$species))))
-# map$logit_cor <- factor(rep(1, length(par$logit_cor)))
-
-obj <- MakeADFun(dat, par, map = map, random = "log_B", DLL = "multispic")
-opt <- nlminb(obj$par, obj$fn, obj$gr,
-              control = list(eval.max = 1000, iter.max = 1000))
-sd_rep <- sdreport(obj)
-rep <- obj$report()
-knitr::kable(data.frame(par = names(opt$par), val = exp(opt$par)))
-sd_rep
-
-## Extract some estimates
-est <- split(unname(sd_rep$value), names(sd_rep$value))
-sd <- split(sd_rep$sd, names(sd_rep$value))
-lwr <- split(unname(sd_rep$value) - 1.96 * sd_rep$sd, names(sd_rep$val))
-upr <- split(unname(sd_rep$value) + 1.96 * sd_rep$sd, names(sd_rep$val))
-index$pred <- exp(est$log_pred_I)
-index$pred_lwr <- exp(lwr$log_pred_I)
-index$pred_upr <- exp(upr$log_pred_I)
-index$std_res <- rep$log_I_std_res
+## Run model
+inputs <- list(landings = landings, index = index)
+fit <- fit_model(inputs, q_groups = "gear_season", cor_str = "one")
+fit$opt$message
 
 ## Explore parameter correlations
+sd_rep <- fit$sd_rep
 dsd <- sqrt(diag(sd_rep$cov.fixed))
 cor_mat <- diag(1 / dsd) %*% sd_rep$cov.fixed %*% diag(1 / dsd)
 rownames(cor_mat) <- paste0(seq(nrow(cor_mat)), ":", names(dsd))
 colnames(cor_mat) <- paste0(seq(nrow(cor_mat)), ":", names(dsd))
 cor_tab <- as.data.frame.table(cor_mat)
 names(cor_tab) <- c("x", "y", "z")
+corrplot::corrplot.mixed(cor_mat, diag = "n", lower = "ellipse", upper = "number")
 cor_tab %>%
     plot_ly(x = ~x, y = ~y, z = ~z, text = ~text,
             colors = c("#B2182B", "white", "#2166AC")) %>%
-    add_heatmap() %>%
-    layout(xaxis = list(title = "", showticklabels = FALSE),
-           yaxis = list(title = "", showticklabels = FALSE))
+    add_heatmap() %>% colorbar(limits = c(-1, 1)) %>%
+    layout(xaxis = list(title = "", showticklabels = TRUE),
+           yaxis = list(title = "", showticklabels = TRUE))
 
 ## Index residuals
-p <- index %>%
+p <- fit$index %>%
     plot_ly(color = ~species, colors = viridis::viridis(100))
 p %>% add_markers(x = ~year, y = ~std_res)
 p %>% add_markers(x = ~log(pred), y = ~std_res)
 p %>% add_markers(x = ~survey, y = ~std_res)
 
 ## Process error residuals
-pe <- data.frame(year = landings$year,
-                 species = landings$species,
-                 stock = landings$stock,
-                 pe = rep$log_B_std_res)
-p <- plot_ly()
-for (nm in unique(pe$species)) {
-    p <- p %>% add_fit(data = pe[pe$species == nm, ],
-                       x = ~year, color = ~species, colors = viridis::viridis(100),
-                       yline = ~pe,
-                       legendgroup = ~species)
-}
-p
+p <- fit$pe %>%
+    plot_ly(color = ~species, colors = viridis::viridis(100))
+p %>% add_lines(x = ~year, y = ~pe)
+
 
 ## Correlation in pe
-pe_wide <- tidyr::spread(pe[, c("year", "species", "pe")], species, pe)
+pe_wide <- tidyr::spread(fit$pe[, c("year", "species", "pe")], species, pe)
 pe_wide$year <- NULL
 plot(pe_wide)
 pe_wide <- as.matrix(pe_wide)
 pe_wide[is.infinite(pe_wide)] <- NA
 cor_mat <- cor(pe_wide, use = "na.or.complete")
-plot_ly(x = rownames(cor_mat), y = rownames(cor_mat), z = ~cor_mat) %>%
-    add_heatmap()
+plot_ly(x = rownames(cor_mat), y = rownames(cor_mat), z = ~cor_mat,
+        colors = c("#B2182B", "white", "#2166AC")) %>%
+    add_heatmap() %>% colorbar(limits = c(-1, 1))
 corrplot::corrplot.mixed(cor_mat, diag = "n", lower = "ellipse", upper = "number")
 
-p <- plot_ly()
-for (nm in levels(index$survey)) {
-    p <- p %>% add_fit(data = index[index$survey == nm, ],
-                       x = ~year, color = ~survey, colors = viridis::viridis(100),
-                       ymin = ~pred_lwr, ymax = ~pred_upr,
-                       ymarker = ~index, yline = ~pred,
-                       legendgroup = ~species)
-}
-p %>% layout(yaxis = list(title = "index"))
-p %>% layout(yaxis = list(type = "log", title = "index"))
 
-
-biomass <- data.frame(year = landings$year,
-                      species = landings$species,
-                      stock = landings$stock,
-                      B = exp(est$log_B_vec) * scaler,
-                      B_lwr = exp(lwr$log_B_vec) * scaler,
-                      B_upr = exp(upr$log_B_vec) * scaler)
-
-p <- plot_ly()
-for (nm in unique(biomass$species)) {
-    p <- p %>% add_fit(data = biomass[biomass$species == nm, ],
-                       x = ~year, color = ~species, colors = viridis::viridis(100),
-                       yline = ~B, ymin = ~B_lwr, ymax = ~B_upr,
-                       legendgroup = ~species)
-}
+## Fits to the index
+p <- fit$index %>%
+    group_by(survey) %>%
+    plot_ly(x = ~year, color = ~species, colors = viridis::viridis(100),
+            legendgroup = ~species) %>%
+    add_ribbons(ymin = ~pred_lwr, ymax = ~pred_upr, line = list(width = 0),
+                alpha = 0.2, showlegend = FALSE) %>%
+    add_lines(y = ~pred) %>%
+    add_markers(y = ~index, showlegend = FALSE)
 p
 p %>% layout(yaxis = list(type = "log"))
 
 
-
+## Biomass
+p <- fit$biomass %>%
+    plot_ly(x = ~year, color = ~species, colors = viridis::viridis(100),
+            legendgroup = ~species) %>%
+    add_ribbons(ymin = ~B_lwr, ymax = ~B_upr, line = list(width = 0),
+                alpha = 0.2, showlegend = FALSE) %>%
+    add_lines(y = ~B)
+p
+p %>% layout(yaxis = list(type = "log"))
 
