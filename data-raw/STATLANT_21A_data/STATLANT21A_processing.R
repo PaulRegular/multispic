5f071e606ef6331156241db070fbe95c2cf332b2
PaulRegular <Paul.Regular@dfo-mpo.gc.ca>
2022-01-21 10:02:54 -0330

    Export and aggregate landings for three regions: 2J3K, 3LNO, and 3Ps

--- data-raw/STATLANT_21A_data/STATLANT21A_processing.R
+++ data-raw/STATLANT_21A_data/STATLANT21A_processing.R
@@ -1,12 +1,22 @@
 
 library(dplyr)
 
-## Import STATLANT 21A landings data from 3LNO
+## Import STATLANT 21A landings data from https://www.nafo.int/Data/STATLANT-21A
 ## Note the landings data are in tonnes
 landings <- read.csv("data-raw/STATLANT_21A_data/STATLANT21A_Extraction.csv")
 names(landings) <- c("year", "country", "division", "species", "landings")
 
-## Groundfish species with all time reported landings of > 1000 tonnes and > 10 years
+## Filter to focal area
+landings <- landings %>%
+    filter(division %in% c("2J", "3K", "3L", "3N", "3O", "3P")) %>%
+    mutate(region = case_when(
+        division %in% c("2J", "3K") ~ "2J3K",
+        division %in% c("3L", "3N", "3O") ~ "3LNO",
+        division %in% c("3P") ~ "3P",
+        TRUE ~ "ERROR"
+    ))
+
+## Demersal fish species with all time reported landings of > 1000 tonnes
 totals <- landings %>%
     group_by(species) %>%
     summarise(n_years = length(unique(year)), total = sum(landings)) %>%
@@ -14,43 +24,52 @@ totals <- landings %>%
 data.frame(totals)
 
 keep_sp <- c("ATLANTIC COD - COD" = "Atlantic Cod",
-             "AMERICAN PLAICE - PLA" = "American Plaice",
              "ATLANTIC REDFISHES (NS) - RED" = "Redfish spp.",
-             "CAPELIN - CAP" = "Capelin",
-             "YELLOWTAIL FLOUNDER - YEL" = "Yellowtail Flounder",
+             "AMERICAN PLAICE - PLA" = "American Plaice",
              "GREENLAND HALIBUT - GHL" = "Greenland Halibut",
-             "SKATES (NS) - SKA" = "Skate spp.",
-             "HADDOCK - HAD" = "Haddock",
+             "YELLOWTAIL FLOUNDER - YEL" = "Yellowtail Flounder",
              "WITCH FLOUNDER - WIT" = "Witch Flounder",
-             "ATLANTIC HERRING - HER" = "Atlantic Herring",
+             "SKATES (NS) - SKA" = "Skate spp.",
+             "ROUNDNOSE GRENADIER - RNG" = "Roundnose Grenadier",
+             "HADDOCK - HAD" = "Haddock",
              "WHITE HAKE - HKW" = "White Hake",
              "WOLFFISHES (CATFISH) (NS) - CAT" = "Wolffish spp.",
              "ROUGHHEAD GRENADIER - RHG" = "Roughhead Grenadier",
              "ATLANTIC HALIBUT - HAL" = "Atlantic Halibut",
-             "ROUNDNOSE GRENADIER - RNG" = "Roundnose Grenadier",
              "AMERICAN ANGLER - ANG" = "Monkfish",
-             "SILVER HAKE - HKS" = "Silver Hake")
+             "RED HAKE - HKR" = "Red Hake",
+             "WINTER FLOUNDER - FLW" = "Winter Flounder", # may not be present in the survey data b/c inshore
+             "SILVER HAKE - HKS" = "Silver Hake",
+             "BEAKED REDFISH(DEEP-WATER) - REB" = "Redfish spp.",
+             "ATLANTIC WOLFFISH - CAA" = "Wolffish spp.",
+             "THORNY SKATE (STARRY RAY) - RJR" = "Skate spp.",
+             "GOLDEN REDFISH - REG" = "Redfish spp.",
+             "NORTHERN WOLFFISH - CAB" = "Wolffish spp.",
+             "SPOTTED WOLFFISH - CAS" = "Wolffish spp.")
 
+## TODO: Consider options for dealing with general FINFISHES (NS), GROUNDFISHES (NS), FLATFISHES (NS) categories
+
+## Subset to demersal fish species with cumulative catch of > 1000 tonnes
 landings <- landings[landings$species %in% names(keep_sp), ]
+landings$species <- keep_sp[as.character(landings$species)] # simplify names
 
-## Sum to 3LNO
+## Sum by region and species
 landings <- landings %>%
-    group_by(species, year) %>%
+    group_by(year, region, species) %>%
     summarise(landings = sum(landings) / 1000) %>%
     as.data.frame()
 
-## Simplify names
-landings$species <- keep_sp[as.character(landings$species)]
-
 ## Fill missing values with zero
-grd <- expand.grid(year = unique(landings$year), species = unique(landings$species))
-landings <- merge(landings, grd, by = c("year", "species"), all = TRUE)
+grd <- expand.grid(year = unique(landings$year),
+                   region = unique(landings$region),
+                   species = unique(landings$species))
+landings <- merge(landings, grd, by = c("year", "region", "species"), all = TRUE)
 landings$landings[is.na(landings$landings)] <- 0
 
 ## Export
-landings$stock <- "3LNO"
-landings <- landings[, c("species", "stock", "year", "landings")]
-names(landings) <- c("Species", "Stock", "Year", "Landings (kt)")
+landings <- landings[, c("year", "species", "region", "landings")]
+landings <- landings[order(landings$year, landings$region, landings$species), ]
+names(landings) <- c("Year", "Species", "Region", "Landings (kt)")
 write.csv(landings, file = "data-raw/landings.csv", row.names = FALSE)
 
 
