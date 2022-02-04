aac4b844d4382c7dd6a0f5f7659c6a8ee1cabda5
PaulRegular <Paul.Regular@dfo-mpo.gc.ca>
2022-02-04 09:02:08 -0330

    start species-specific analysis code

--- analysis/002_NL_case_study_analysis.R
+++ analysis/002_NL_case_study_analysis.R
@@ -100,4 +100,44 @@ fits$no_temporal_cor$retro$mse
 
 
 
+## Run species-specific analyses
 
+for (r in c("2J3K", "3LNO", "3Ps")) {
+
+    fits <- readRDS(paste0("analysis/exports/fits_", r, ".rds"))
+    spp <- levels(fits$full$landings$species)
+
+    for (sp in spp) {
+
+        sp <- gsub(paste0("-", r), "", sp)
+        sp <- c("Atlantic Cod", "American Plaice")
+        sp <- "Atlantic Cod"
+        r <- "2J3K"
+        list2env(nl_inputs_and_priors(region = r, species = sp), envir = globalenv())
+
+        null <- multispic(inputs, species_cor = "none", temporal_cor = "none",
+                          log_K_option = par_option(option = "normal_prior",
+                                                    mean = mean_log_K, sd = sd_log_K),
+                          log_B0_option = par_option(option = "normal_prior",
+                                                     mean = mean_log_B0, sd = sd_log_B0),
+                          log_r_option = par_option(option = "normal_prior",
+                                                    mean = mean_log_r, sd = sd_log_r),
+                          log_sd_B_option = par_option(option = "normal_prior",
+                                                       mean = mean_log_sd_B, sd = sd_log_sd_B),
+                          log_q_option = par_option(option = "normal_prior",
+                                                    mean = mean_log_q, sd = sd_log_q),
+                          log_sd_I_option = par_option(option = "normal_prior",
+                                                       mean = mean_log_sd_I, sd = sd_log_sd_I),
+                          logit_rho_option = par_option(option = "normal_prior",
+                                                        mean = mean_logit_rho, sd = sd_logit_rho),
+                          logit_phi_option = par_option(option = "normal_prior",
+                                                        mean = mean_logit_phi, sd = sd_logit_phi),
+                          n_forecast = 1, K_groups = NULL, pe_covariates = NULL)
+
+    }
+
+
+
+
+
+}
