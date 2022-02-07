f146a8477900a845317ffbdb0ee1da0e30cdbc02
PaulRegular <Paul.Regular@dfo-mpo.gc.ca>
2022-02-07 16:23:56 -0330

    Run a few more checks on survey_group option; gear + season * species appears to be the best and makes sense

--- analysis/survey_group_tests.R
+++ analysis/survey_group_tests.R
@@ -2,30 +2,31 @@
 source("analysis/001_NL_case_study_helpers.R")
 
 
-list2env(nl_inputs_and_priors(region = "3LNO", species = NULL), envir = globalenv())
+list2env(nl_inputs_and_priors(region = "3LNO",
+                              species = c("Atlantic Cod", "American Plaice", "Redfish spp.")), envir = globalenv())
 
 ## Dev notes:
 ## - Forcing the RW structure results in unusual process errors for some species
 ## - During testing the fixed, random, and uniform_prior options rarely converged
-int_eff <- multispic(inputs, species_cor = "all", temporal_cor = "ar1",
-                  log_K_option = par_option(option = "normal_prior",
-                                            mean = mean_log_K, sd = sd_log_K),
-                  log_B0_option = par_option(option = "normal_prior",
-                                             mean = mean_log_B0, sd = sd_log_B0),
-                  log_r_option = par_option(option = "normal_prior",
-                                            mean = mean_log_r, sd = sd_log_r),
-                  log_sd_B_option = par_option(option = "normal_prior",
-                                               mean = mean_log_sd_B, sd = sd_log_sd_B),
-                  log_q_option = par_option(option = "normal_prior",
-                                            mean = mean_log_q, sd = sd_log_q),
-                  log_sd_I_option = par_option(option = "normal_prior",
-                                               mean = mean_log_sd_I, sd = sd_log_sd_I),
-                  logit_rho_option = par_option(option = "normal_prior",
-                                                mean = mean_logit_rho, sd = sd_logit_rho),
-                  logit_phi_option = par_option(option = "normal_prior",
-                                                mean = mean_logit_phi, sd = sd_logit_phi),
-                  n_forecast = 1, K_groups = ~1, survey_groups = ~species * gear * season,
-                  pe_covariates = ~0)
+int_eff <- multispic(inputs, species_cor = "none", temporal_cor = "ar1",
+                     log_K_option = par_option(option = "normal_prior",
+                                               mean = mean_log_K, sd = sd_log_K),
+                     log_B0_option = par_option(option = "normal_prior",
+                                                mean = mean_log_B0, sd = sd_log_B0),
+                     log_r_option = par_option(option = "normal_prior",
+                                               mean = mean_log_r, sd = sd_log_r),
+                     log_sd_B_option = par_option(option = "normal_prior",
+                                                  mean = mean_log_sd_B, sd = sd_log_sd_B),
+                     log_q_option = par_option(option = "normal_prior",
+                                               mean = mean_log_q, sd = sd_log_q),
+                     log_sd_I_option = par_option(option = "normal_prior",
+                                                  mean = mean_log_sd_I, sd = sd_log_sd_I),
+                     logit_rho_option = par_option(option = "normal_prior",
+                                                   mean = mean_logit_rho, sd = sd_logit_rho),
+                     logit_phi_option = par_option(option = "normal_prior",
+                                                   mean = mean_logit_phi, sd = sd_logit_phi),
+                     n_forecast = 1, K_groups = ~1, survey_groups = ~species * gear + species * season,
+                     pe_covariates = ~0)
 
 
 mix_eff <- update(int_eff, survey_groups = ~gear + species * season)
@@ -53,7 +54,7 @@ int_eff$retro$mse
 mix_eff$retro$mse
 main_eff$retro$mse
 
-fit <- main_eff
+fit <- int_eff
 
 if (fit$call$K_groups == ~1) {
     K_groups <- NULL
