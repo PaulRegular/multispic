7de05b5e676f116a657a52681547ce44dda62014
PaulRegular <Paul.Regular@dfo-mpo.gc.ca>
2022-02-09 12:02:29 -0330

    Add population trend plots to dashboard

--- analysis/vis_multispic_tests.R
+++ analysis/vis_multispic_tests.R
@@ -2,10 +2,15 @@
 source("analysis/001_NL_case_study_helpers.R")
 
 
-list2env(nl_inputs_and_priors(region = "3LNO", species = NULL), envir = globalenv())
+list2env(nl_inputs_and_priors(region = "2J3K", species = NULL), envir = globalenv())
 
+inputs$landings$spp_group <- ifelse(inputs$landings$species %in% c("Atlantic Cod-2J3K"),
+                                    "cod", "not-cod")
 
-fit <- multispic(inputs, species_cor = "none", temporal_cor = "none",
+inputs$index$spp_group <- ifelse(inputs$index$species %in% c("Atlantic Cod-2K3K"),
+                                 "cod", "not-cod")
+
+fit <- multispic(inputs, species_cor = "none", temporal_cor = "AR1",
                  log_K_option = par_option(option = "normal_prior",
                                            mean = mean_log_K, sd = sd_log_K),
                  log_B0_option = par_option(option = "normal_prior",
@@ -22,8 +27,8 @@ fit <- multispic(inputs, species_cor = "none", temporal_cor = "none",
                                                mean = mean_logit_rho, sd = sd_logit_rho),
                  logit_phi_option = par_option(option = "normal_prior",
                                                mean = mean_logit_phi, sd = sd_logit_phi),
-                 n_forecast = 1, K_groups = ~1, survey_groups = ~gear + season * species,
-                 pe_covariates = ~winter_nao)
+                 n_forecast = 1, K_groups = ~spp_group, survey_groups = ~gear + species,
+                 pe_covariates = ~0)
 
 vis_multispic(fit)
 
