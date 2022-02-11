68ff4dddea0bcce48c396952c525ac0ff51daeae
PaulRegular <Paul.Regular@dfo-mpo.gc.ca>
2022-02-10 21:52:42 -0330

    Minor improvements to exploration of cv results

--- analysis/004_NL_case_study_plots.R
+++ analysis/004_NL_case_study_plots.R
@@ -8,9 +8,9 @@ normalize <- function(x) {(x - min(x)) / (max(x) - min(x)) }
 
 ## Export select dashboards ------------------------------------------------------------------------
 
-spp_fits <- readRDS("analysis/exports/spp_fits_2J3K.rds")
-fit <- spp_fits$one_species_cor
-vis_multispic(fit, output_file = "analysis/exports/dashboards/spp_fit_2J3KL_one_species_cor.html")
+# spp_fits <- readRDS("analysis/exports/spp_fits_2J3K.rds")
+# fit <- spp_fits$one_species_cor
+# vis_multispic(fit, output_file = "analysis/exports/dashboards/spp_fit_2J3KL_one_species_cor.html")
 
 
 ## Leave-one-out results ---------------------------------------------------------------------------
@@ -27,6 +27,7 @@ loo_dat <- lapply(c("2J3K", "3LNO", "3Ps"), function(r) {
         spp_fit <- spp_fits[[m]]
         data.frame(model = names(models)[models == m],
                    year = spp_fit$index$year, species = spp_fit$index$species,
+                   survey = spp_fit$index$survey,
                    obs = spp_fit$loo$obs, pred = spp_fit$loo$pred)
     })
     spp_loo_dat <- do.call(rbind, spp_loo_dat)
@@ -38,6 +39,7 @@ loo_dat <- lapply(c("2J3K", "3LNO", "3Ps"), function(r) {
             if (sp_fit$sd_rep$pdHess) {
                 data.frame(model = "Single-species",
                            year = sp_fit$index$year, species = sp_fit$index$species,
+                           survey = sp_fit$index$survey,
                            obs = sp_fit$loo$obs, pred = sp_fit$loo$pred)
             }
         }
@@ -56,6 +58,16 @@ sr <- do.call(rbind, strsplit(as.character(loo_dat$species), "-"))
 loo_dat$species <- sr[, 1]
 loo_dat$region <- sr[, 2]
 
+## limit to species that converged in the single-species analysis
+sp_loo_groups <- loo_dat %>%
+    filter(model == "Single-species") %>%
+    mutate(ysr = paste0(year, "-", species, "-", region))
+sp_loo_groups <- unique(sp_loo_groups$ysr)
+
+loo_dat <- loo_dat %>%
+    mutate(ysr = paste0(year, "-", species, "-", region)) %>%
+    filter(ysr %in% sp_loo_groups)
+
 loo_scores <- loo_dat %>%
     group_by(model, species, region) %>%
     summarise(n = n(), n_species = length(unique(species)),
@@ -65,14 +77,7 @@ loo_scores <- loo_dat %>%
            delta_rmse = rmse - min(rmse)) %>%
     ungroup()
 
-sp_loo_groups <- loo_dat %>%
-    filter(model == "Single-species") %>%
-    mutate(ysr = paste0(year, "-", species, "-", region))
-sp_loo_groups <- unique(sp_loo_groups$ysr)
-
 overall_loo_scores <- loo_dat %>%
-    mutate(ysr = paste0(year, "-", species, "-", region)) %>%
-    filter(ysr %in% sp_loo_groups) %>%  # limit to species that converged in the single-species analysis
     group_by(model, region) %>%
     summarise(n = n(), n_species = length(unique(species)),
               rmse = sqrt(mean((obs - pred) ^ 2, na.rm = TRUE))) %>%
@@ -81,9 +86,9 @@ overall_loo_scores <- loo_dat %>%
            delta_rmse = rmse - min(rmse)) %>%
     ungroup()
 
+
 loo_scores %>%
     filter(region == "2J3K") %>%
-    filter(!model %in% c("No NAO", "Just NAO")) %>%
     mutate(model = factor(model)) %>%
     plot_ly(x = ~model, y = ~delta_rmse, color = ~species, frame = ~region,
             colors = viridis::viridis(100)) %>%
@@ -91,7 +96,6 @@ loo_scores %>%
 
 loo_scores %>%
     filter(region == "3LNO") %>%
-    filter(!model %in% c("No NAO", "Just NAO")) %>%
     mutate(model = factor(model)) %>%
     plot_ly(x = ~model, y = ~delta_rmse, color = ~species, frame = ~region,
             colors = viridis::viridis(100)) %>%
@@ -99,7 +103,6 @@ loo_scores %>%
 
 loo_scores %>%
     filter(region == "3Ps") %>%
-    filter(!model %in% c("No NAO", "Just NAO")) %>%
     mutate(model = factor(model)) %>%
     plot_ly(x = ~model, y = ~delta_rmse, color = ~species, frame = ~region,
             colors = viridis::viridis(100)) %>%
@@ -107,7 +110,6 @@ loo_scores %>%
 
 
 overall_loo_scores %>%
-    filter(!model %in% c("No NAO", "Just NAO")) %>%
     mutate(model = factor(model)) %>%
     plot_ly(x = ~model, y = ~delta_rmse, color = ~region,
             colors = viridis::viridis(100)) %>%
@@ -150,6 +152,16 @@ sr <- do.call(rbind, strsplit(as.character(hind_dat$species), "-"))
 hind_dat$species <- sr[, 1]
 hind_dat$region <- sr[, 2]
 
+## limit to species that converged in the single-species analysis
+sp_hind_groups <- hind_dat %>%
+    filter(model == "Single-species") %>%
+    mutate(ysr = paste0(year, "-", species, "-", region))
+sp_hind_groups <- unique(sp_hind_groups$ysr)
+
+hind_dat <- hind_dat %>%
+    mutate(ysr = paste0(year, "-", species, "-", region)) %>%
+    filter(ysr %in% sp_hind_groups)
+
 hind_scores <- hind_dat %>%
     group_by(model, species, region) %>%
     summarise(n = n(), n_species = length(unique(species)),
@@ -159,14 +171,7 @@ hind_scores <- hind_dat %>%
            delta_rmse = rmse - min(rmse)) %>%
     ungroup()
 
-sp_hind_groups <- hind_dat %>%
-    filter(model == "Single-species") %>%
-    mutate(ysr = paste0(year, "-", species, "-", region))
-sp_hind_groups <- unique(sp_hind_groups$ysr)
-
 overall_hind_scores <- hind_dat %>%
-    mutate(ysr = paste0(year, "-", species, "-", region)) %>%
-    filter(ysr %in% sp_hind_groups) %>%  # limit to species that converged in the single-species analysis
     group_by(model, region) %>%
     summarise(n = n(), n_species = length(unique(species)),
               rmse = sqrt(mean((log_index - log_pred_index) ^ 2, na.rm = TRUE))) %>%
@@ -175,8 +180,24 @@ overall_hind_scores <- hind_dat %>%
            delta_rmse = rmse - min(rmse)) %>%
     ungroup()
 
-## May not be plotting correctly given different species in different regions
+
 hind_scores %>%
+    filter(region == "2J3K") %>%
+    mutate(model = factor(model)) %>%
+    plot_ly(x = ~model, y = ~delta_rmse, color = ~species, frame = ~region,
+            colors = viridis::viridis(100)) %>%
+    add_lines()
+
+hind_scores %>%
+    filter(region == "3LNO") %>%
+    mutate(model = factor(model)) %>%
+    plot_ly(x = ~model, y = ~delta_rmse, color = ~species, frame = ~region,
+            colors = viridis::viridis(100)) %>%
+    add_lines()
+
+hind_scores %>%
+    filter(region == "3Ps") %>%
+    mutate(model = factor(model)) %>%
     plot_ly(x = ~model, y = ~delta_rmse, color = ~species, frame = ~region,
             colors = viridis::viridis(100)) %>%
     add_lines()
