57ec2c4524e6e39720c43d8fc0c088e4a5aae767
PaulRegular <Paul.Regular@dfo-mpo.gc.ca>
2022-04-06 09:48:34 -0230

    Organize plotting scripts

--- analysis/NL_case_study/007_plot_pop_trends.R
+++ analysis/NL_case_study/007_plot_pop_trends.R
@@ -1,67 +1,7 @@
 
 library(plotly)
 
-save_html <- function(p, file = file, selfcontained = TRUE, ...) {
-    ext <- tools::file_ext(file)
-    tmp_file <- paste0("tmp.", ext)
-    htmlwidgets::saveWidget(partial_bundle(p), file = tmp_file,
-                            selfcontained = selfcontained, ...)
-    invisible(file.rename(tmp_file, file))
-}
-
-## Plot trends estimated by the just correlation model
-
-fits_2J3K <- readRDS("analysis/NL_case_study/exports/spp_fits_2J3K.rds")
-fit_2J3K <- fits_2J3K$just_cor
-
-fits_3LNO <- readRDS("analysis/NL_case_study/exports/spp_fits_3LNO.rds")
-fit_3LNO <- fits_3LNO$just_cor
-
-fits_3Ps <- readRDS("analysis/NL_case_study/exports/spp_fits_3Ps.rds")
-fit_3Ps <- fits_3Ps$just_cor
-
-spp_2J3K <- gsub("-2J3K", "", levels(fit_2J3K$landings$species))
-spp_3LNO <- gsub("-3LNO", "", levels(fit_3LNO$landings$species))
-spp_3Ps <- gsub("-3Ps", "", levels(fit_3Ps$landings$species))
-
-all_spp <- c(spp_2J3K, spp_3LNO, spp_3Ps)|>
-    unique() |>
-    sort()
-all_spp
-
-## Rough phylogenetic order + limit species to improve visual contrast
-
-# all_spp <- c("Redfish spp.", "Wolffish spp.",
-#              "Yellowtail Flounder", "Witch Flounder", "American Plaice", "Greenland Halibut",
-#              "White Hake", "Haddock", "Atlantic Cod",
-#              "Skate spp.")
-all_spp <- c("Redfish spp.",
-             "Yellowtail Flounder", "American Plaice", "Greenland Halibut",
-             "Atlantic Cod",
-             "Skate spp.")
-spp_cols <- viridis::viridis(length(all_spp))
-# see::material_colors()
-# spp_cols <- see::material_colors() |> head(length(all_spp))
-# spp_cols <- see::material_colors()[c("red", "pink",
-#                                      "yellow", "brown", "deep purple", "green",
-#                                      "purple", "teal", "blue",
-#                                      "amber")]
-names(spp_cols) <- all_spp
-spp_cols
-
-fit_2J3K$pop$species <- gsub("-2J3K", "", fit_2J3K$pop$species)
-fit_2J3K$pop$species <- factor(fit_2J3K$pop$species, levels = all_spp)
-
-fit_3LNO$pop$species <- gsub("-3LNO", "", fit_3LNO$pop$species)
-fit_3LNO$pop$species <- factor(fit_3LNO$pop$species, levels = all_spp)
-
-fit_3Ps$pop$species <- gsub("-3Ps", "", fit_3Ps$pop$species)
-fit_3Ps$pop$species <- factor(fit_3Ps$pop$species, levels = all_spp)
-
-## create dummy data for legend
-dummy_data <- expand.grid(year = min(fit_2J3K$tot_pop$year), x = 0,
-                          species = factor(all_spp, levels = all_spp))
-
+source("analysis/NL_case_study/006_compile_trends.R")
 
 region_trends <- function(fit, title, show_legend = TRUE, show_axis_titles = TRUE) {
 
@@ -149,14 +89,6 @@ save_image(p, file = "analysis/NL_case_study/exports/plots/pop_trends.svg",
            width = 1000, height = 700)
 save_html(p, file = "analysis/NL_case_study/exports/plots/pop_trends.html")
 saveRDS(p, file = "analysis/NL_case_study/exports/plots/pop_trends.rds")
-
-
-## Consider exporting and displaying density dependent subtractions
-## along with additions and subtractions from process error
-## And subtractions from landings
-
-## Figure out how to display uncertainty without making the plot too busy
-
 
 
 
---
+++ analysis/NL_case_study/008_plot_survey_trends.R
@@ -0,0 +1,9 @@
+
+library(plotly)
+
+source("analysis/NL_case_study/006_compile_trends.R")
+
+
+
+
+
