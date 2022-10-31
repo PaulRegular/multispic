02a082a00108ec7094417f9a6a7b23dacabd892f
PaulRegular <Paul.Regular@dfo-mpo.gc.ca>
2022-10-31 10:57:52 -0230

    Rename dashboard names and zip for supplement

--- analysis/NL_case_study/005_export_dashboards.R
+++ analysis/NL_case_study/005_export_dashboards.R
@@ -1,13 +1,22 @@
 
 ## Export select dashboards ------------------------------------------------------------------------
 
+region_name <- c("2J3K" = "Northeast NL Shelf",
+                 "3LNO" = "Grand Bank",
+                 "3Ps" = "Southern NL")
+
 ## Just correlation model as it is generally ranked high
 for (r in c("2J3K", "3LNO", "3Ps")) {
+
     fits <- readRDS(paste0("analysis/NL_case_study/exports/fits_", r, ".rds"))
+
     vis_multispic(fits$null,
-                  output_file = paste0("analysis/NL_case_study/exports/dashboards/null_", r, ".html"))
+                  output_file = paste0("analysis/NL_case_study/exports/dashboards/Single-species - ", region_name[r], ".html"))
     vis_multispic(fits$just_cor,
-                  output_file = paste0("analysis/NL_case_study/exports/dashboards/just_cor_", r, ".html"))
+                  output_file = paste0("analysis/NL_case_study/exports/dashboards/Just correlation - ", region_name[r], ".html"))
+
 }
 
-
+zip("analysis/paper/supplement/model_dashboards.zip",
+    list.files("analysis/NL_case_study/exports/dashboards/", full.names = TRUE),
+    extras = "-j")
